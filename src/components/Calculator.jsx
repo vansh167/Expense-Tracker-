bd351b2f0283dc25ec6965ad7bfdedd31909c255
Vansh <vanshdoc7@gmail.com>
2025-12-04 16:23:18 +0530

    005

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -3,7 +3,7 @@ import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import { format, parseISO, isValid } from 'date-fns';
 import '../style/Calculator.css';
-import { Parser } from 'expr-eval'; // <- added
+import { Parser } from 'expr-eval'; 
 
 function safeParseDate(dateString) {
   if (!dateString) return null;
